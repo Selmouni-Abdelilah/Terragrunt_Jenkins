74984b670814ec036155a12f29099b092e06b07e
Selmouni-Abdelilah <128396251+Selmouni-Abdelilah@users.noreply.github.com>
2024-01-14 23:42:26 +0100

    Update outputs.tf

--- modules/app_services/outputs.tf
+++ modules/app_services/outputs.tf
@@ -1,5 +1,5 @@
 output "webapp_name" {
-  value       = azurerm_linux_web_app.webapp.default_site_hostname
+  value       = azurerm_linux_web_app.webapp.default_hostname
   description = "The default hostname of the web app."
 }
 output "id" {
