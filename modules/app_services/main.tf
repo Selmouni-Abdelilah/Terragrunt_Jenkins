aa6aeba2918532a6764630ed8846bd06dd71e1d9
Selmouni-Abdelilah <128396251+Selmouni-Abdelilah@users.noreply.github.com>
2024-01-14 23:24:22 +0100

    Update main.tf

--- modules/app_services/main.tf
+++ modules/app_services/main.tf
@@ -1,29 +1,28 @@
-resource "azurerm_app_service_plan" "app_service_plan" {
+# Create the Linux App Service Plan
+resource "azurerm_service_plan" "appserviceplan" {
   name                = var.app_service_plan_name
   location            = var.location
   resource_group_name = var.rg_name
-
-  sku {
-    tier = "Standard"
-    size = "S1"
-  }
+  os_type             = "Linux"
+  sku_name            = "B1"
 }
-
-resource "azurerm_app_service" "webapp" {
+# Create the web app
+resource "azurerm_linux_web_app" "webapp" {
   name                = var.app_service_name
   location            = var.location
   resource_group_name = var.rg_name
-  app_service_plan_id = azurerm_app_service_plan.app_service_plan.id
-
-  site_config {
-    dotnet_framework_version = "v6.0"
-  }
-
-  source_control {
-    repo_url           = var.repo_url
-    branch             = var.branch
-    manual_integration = true
-    use_mercurial      = false
+  service_plan_id       = azurerm_service_plan.appserviceplan.id
+  https_only            = true
+  site_config { 
+    minimum_tls_version = "1.2"
   }
 }
 
+#  Deploy code from GitHub repo
+resource "azurerm_app_service_source_control" "sourcecontrol" {
+  app_id             = azurerm_linux_web_app.webapp.id
+  repo_url           = var.repo_url
+  branch             = var.branch
+  use_manual_integration = true
+  use_mercurial      = false
+}
